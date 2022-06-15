c1b9d094168ab1956b68a8f2a6677c2388744fff
Redboyrave <gabrielgreis94@hotmail.com>
2022-06-15 01:11:13 -0300

    Update to the Player Prefab + Re-written movement script

--- Assets/Player/Scripts/Movement.cs
+++ Assets/Player/Scripts/Movement.cs
@@ -1,135 +1,165 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
-
 {
-    public CharacterController c_ctrler;
-    public Transform cam; //Camera Reference for Rotations
-    public Animator anim_manager; //Animator Reference
-    public PlayerInfo p_info; //Reference to Player Info Scriptable Object.<- Keeps Track of player related stats.
-    public float speed = 5f; //Default move speed
-    public float crouch_speed = 2f; //Crouched move speed
-    public float dash_speed = 8f; //Dashing move speed
-    public float turn_speed = 0.1f; //TurningSpeed
-    private float turn_speed_velocity; //Necessary for DampedRotation Function
-    private InputMaster _input; //unity new input system class. Contains Keybinds
-    private Vector2 input_dir; //Vector 2 to represent Input Direction from controllers
-    private Vector3 move_dir; // Vector3 that indicates movemente direction
+    //References to other objects
+    private PlayerInputManager _input; 
+    private Rigidbody rb;
+    private Transform _camera_ref;
+    private MakeNoise _noise;
+    private CapsuleCollider cap_col;
+    private CanClimbCheck _ledge;
 
-    //Bools for velocity and change//
-    private Boolean is_dashing=false; 
-    private Boolean is_crouching=false;
-
-    //HASH VALUES FOR ANIMATIONS//
-    int crouch_hash;
-    int dash_hash;
-    int vel_hash;
-
-    private void Awake(){
-        _input = new InputMaster(); //new Instance of the InputSystem
-    }
-
-    private void Start(){
-    _input.Player.Crouch.performed += crouch;
-    _input.Player.Dash.performed += start_dash;
-    _input.Player.Dash.canceled += stop_dash;
-
-    //CONVERT TO HASH THE STRINGS//
-    crouch_hash = Animator.StringToHash("Is_crouching");
-    dash_hash = Animator.StringToHash("Is_dashing");
-    vel_hash = Animator.StringToHash("Movement_speed");
-
-    }
+    [Header("Movement Variables")]
+    [SerializeField]private float walk_speed = 5f;
+    [SerializeField]private float crouch_speed = 2.5f;
+    [SerializeField]private float dash_speed = 8f;
+    [SerializeField]private float JumpHeight = 3f;
+    [SerializeField]private float JumpTime = .5f;
+    [SerializeField]private float JumpAceleration = .5f;
+    // [SerializeField]private float GravityMultiplier = 2.5f;
+    private float turn_velocity; //Used for rotation and stuffs don't mind it
+    private float dist_to_ground;
+    private float vertical_speed;
+    [HideInInspector]public bool is_grounded = false;
+    [HideInInspector]public bool is_dashing = false;
+    [HideInInspector]public bool is_crouched = false;
+    [HideInInspector]public bool is_hanging = false;
 
 
-    private void crouch(InputAction.CallbackContext obj)
+
+    // Start is called before the first frame update
+    void Start()
     {
-        is_crouching = !is_crouching;
-        anim_manager.SetBool(crouch_hash,is_crouching);
-        if(is_crouching){
+        //Get References
+        _input = GetComponent<PlayerInputManager>();
+        rb = GetComponent<Rigidbody>();
+        _noise = GetComponent<MakeNoise>();
+        _ledge = GetComponentInChildren<CanClimbCheck>();
+        _camera_ref = _input._camera;
+        cap_col = GetComponent<CapsuleCollider>();
+
+        rb.isKinematic = false;// Ensures that the RigidBody is Physics enabled
+        remove_child_collision();//Stops collisions between main collider and children colliders
+
+        dist_to_ground = cap_col.bounds.extents.y - cap_col.center.y; //Calculates distance to ground for GroundCheck
+        Debug.Log(_ledge);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        is_grounded = check_ground(); //Checks if Player is grounded (Perform every frame pls)
+        
+        movement();
+        if (_ledge.IsGrabingLedge){
+            wall_hang();         
+        }
+    }
+    
+    private float speed_check(){
+        float movement_speed;
+        if(is_dashing){
+            movement_speed = dash_speed;
+        }
+        else if (is_crouched){
+            movement_speed = crouch_speed;
+        }
+        else{
+            movement_speed = walk_speed;
+        }
+        return movement_speed;
+    }
+    void movement(){
+        Vector2 input_dir = _input.JoystickInput;
+        Vector3 move_dir;
+        float movement_speed = speed_check();
+
+        //XZ PLANE MOVEMENT
+        if (input_dir.magnitude >= 0.1f){
+            float targetAngle = Mathf.Atan2(input_dir.x,input_dir.y) * Mathf.Rad2Deg + _camera_ref.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y,targetAngle,ref turn_velocity,Time.deltaTime);
+            Quaternion q_rot = Quaternion.Euler(0,angle,0);
+            move_dir = q_rot * Vector3.forward; // Rotates the forward vector to the angle of the camera.
+            rb.MoveRotation(q_rot);
+        }
+        else {move_dir= Vector3.zero;}; 
+        
+        _noise.Call_Noise(move_dir.magnitude);
+
+        //Jumping Logic
+        if (_input.Jump && is_grounded){
+            Debug.Log("Jump!");
+            vertical_speed =  JumpHeight/JumpTime - JumpAceleration*JumpTime/2f;
+            // Equation derived from h = Vot + at²/2
+            // Isolating Vo, we get Vo = h/t - at/2
+        }
+        else if (!is_grounded){
+            vertical_speed += Physics.gravity.y*Time.deltaTime;
+            // Debug.Log(vertical_speed);
+        }
+        else {vertical_speed = 0f;}
+
+        rb.velocity = new Vector3(move_dir.x*movement_speed,vertical_speed,move_dir.z*movement_speed);
+
+    }
+    
+    void wall_hang(){
+        is_hanging = true;
+        rb.isKinematic = true;
+        RaycastHit wall_hit = raycast_forward();
+        rb.MoveRotation(Quaternion.Euler(wall_hit.normal));
+        
+
+    }
+
+    public void crouch(UnityEngine.InputSystem.InputAction.CallbackContext obj){
+        is_crouched = !is_crouched;
+        if(is_crouched){
             char_crouch();
         }
         else{
             char_stand();
         }
-        
+        Debug.Log("Crouch Command");
     }
-    private void start_dash(InputAction.CallbackContext obj)
-    {
-        is_dashing = true;
-        anim_manager.SetBool(dash_hash,is_dashing);
-        if (is_crouching){
-            is_crouching = false;
-            anim_manager.SetBool(crouch_hash,is_crouching);
-            char_stand();
+
+    private void remove_child_collision(){
+        Collider[] cols = GetComponentsInChildren<Collider>();
+        foreach (Collider col in cols){
+            if (col == GetComponent<Collider>()){
+                Debug.Log("I've found Myself");
+                continue;
+            }
+            Physics.IgnoreCollision(GetComponent<Collider>(),col,true);
         }
     }
-    private void stop_dash(InputAction.CallbackContext obj)
-    {
-        is_dashing = false;
-        anim_manager.SetBool(dash_hash,is_dashing);
+
+    private bool check_ground(){
+        return Physics.Raycast(transform.position,Vector3.down, dist_to_ground);
     }
 
-    // private void sound_debug(InputAction.CallbackContext obj)
-    // {
-    //     MakeNoise soundmaker = col.GetComponent<MakeNoise>();
-    //     soundmaker.Call_Noise(UnityEngine.Random.Range(5,50));
-    // }
-    private void OnEnable(){
-        _input.Player.Enable(); //Enables the Input
-
-    }
-    private void OnDisable(){
-        _input.Player.Disable(); //Disables the Input
-    }
-
-    private void FixedUpdate(){
-        input_dir = _input.Player.Move.ReadValue<Vector2>(); //Reads the value from input and stores them as Vector2
-        //DEBUG
-        // Debug.Log("Controllers are Outputing " + input_dir);    
-
-
-        if (input_dir.magnitude >= 0.1f){    //Checks the magnitude of input to move (avoids moving to 0,0 always + adds deadzone)
-            float targetAngle = Mathf.Atan2(input_dir.x,input_dir.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turn_speed_velocity,turn_speed);
-            // Debug.LogFormat("Angle is " + angle.ToString());
-            transform.rotation = Quaternion.Euler(0,angle,0);
-            Vector3 move_dir = Quaternion.Euler(0,angle,0) * Vector3.forward; // Rotates the forward vector to the angle of the camera.
-           
-            anim_manager.SetFloat(vel_hash,move_dir.normalized.magnitude);
-            Vector3 movement;
-            if (is_dashing){
-                movement =move_dir.normalized*dash_speed*Time.deltaTime; //Higher speed for the dash move
-            }
-            else if (is_crouching){
-                movement = move_dir.normalized*crouch_speed*Time.deltaTime; //Lower Speed for the crouching movement
-            }
-            else{
-                movement = move_dir.normalized*speed*Time.deltaTime; //The base move speed
-            }
-            GetComponent<MakeNoise>().Call_Noise(movement.magnitude*20);
-            c_ctrler.Move(movement); //The actual move function - Takes direction*speed*framerate
-        }
-    anim_manager.SetFloat(vel_hash,input_dir.magnitude);
-    p_info.PlayerCurrentCoordinates = this.transform.position;
-        
+    private RaycastHit raycast_forward(){ 
+        Ray f_ray = new Ray(transform.position,transform.forward);
+        RaycastHit hitdata;
+        Physics.Raycast(transform.position,transform.forward,out hitdata,1f);
+        return hitdata;
     }
 
 
-    //Functions for changing the Character Controller Collision Size when Crouching and Standing Up
+    //Functions for changing the Capsule Collision Size when Crouching and Standing Up
     private void char_crouch(){
-        c_ctrler.height = 1.2f;
-        c_ctrler.radius = 0.35f;
-        c_ctrler.center = new Vector3(0,0.6f,0);
+        cap_col.height = 1.2f;
+        cap_col.radius = 0.35f;
+        cap_col.center = new Vector3(0,0.6f,0);
+        dist_to_ground = cap_col.bounds.extents.y - cap_col.center.y;
     }
     private void char_stand(){
-        c_ctrler.height =1.85f;
-        c_ctrler.radius = 0.25f;
-        c_ctrler.center = new Vector3(0,0.9f,0);
+        cap_col.height =1.82f;
+        cap_col.radius = 0.2f;
+        cap_col.center = new Vector3(0,0.9f,0);
+        dist_to_ground = cap_col.bounds.extents.y - cap_col.center.y;
     }
 }
-
